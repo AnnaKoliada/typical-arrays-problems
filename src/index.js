9a90dcafffa9463c999e543b23992c98ec001409
AnnaKoliada <anacirec@gmail.com>
2021-03-10 12:40:00 +0300

    feat: tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+  if (!array || !array.length) {
+    return 0;
+  }
 
-exports.min = function min (array) {
-  return 0;
-}
+  let min = array[0];
 
-exports.max = function max (array) {
-  return 0;
-}
+  for (i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  let max = array[0];
+
+  for (i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+};
+
+exports.avg = function avg(array) {
+  if (!array || !array.length) {
+    return 0;
+  }
+  let sum = 0;
+  for (i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
+};
